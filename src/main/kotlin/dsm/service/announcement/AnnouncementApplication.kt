49a27eea49e34651db1cc7a72edfe9ea7ad869ec
Maly <migsking@naver.com>
2020-09-23 01:51:34 +0900

    [ADD] <AnnouncementApplicaiton.kt> 서비스 등록

--- src/main/kotlin/dsm/service/announcement/AnnouncementApplication.kt
+++ src/main/kotlin/dsm/service/announcement/AnnouncementApplication.kt
@@ -1,23 +1,26 @@
 package dsm.service.announcement
 
+import dsm.service.announcement.presentation.service.AnnouncementService
 import dsm.service.announcement.proto.AnnouncementServiceGrpcKt
 import io.grpc.ServerBuilder
 
 class AnnouncementApplication constructor(
-    private val port: Int
+    private val port: Int,
+    private val announcementService: AnnouncementService
 ) {
     val server = ServerBuilder
         .forPort(port)
+        .addService(announcementService)
         .build()
 
     fun start() {
         server.start()
-        println("Server started, listening on $port")
+        println("* Server started, listening on $port")
         Runtime.getRuntime().addShutdownHook(
             Thread {
-                println("*** shutting down gRPC server since JVM is shutting down")
+                println("* shutting down gRPC server since JVM is shutting down")
                 this@AnnouncementApplication.stop()
-                println("*** server shut down")
+                println("* server shut down")
             }
         )
     }
@@ -33,7 +36,8 @@ class AnnouncementApplication constructor(
 
 fun main() {
     val port = 10000
-    val server = AnnouncementApplication(port)
+    val announcementService = AnnouncementService()
+    val server = AnnouncementApplication(port, announcementService)
     server.start()
     server.blockUntilShutdown()
 }
