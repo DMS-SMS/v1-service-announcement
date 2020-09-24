c85fa474562018aa8f95ec01296bb31fd4b5c402
Maly <migsking@naver.com>
2020-09-24 19:37:56 +0900

    [MODIFY] Application빼고 다 제거

--- src/main/kotlin/dsm/service/announcement/AnnouncementApplication.kt
+++ src/main/kotlin/dsm/service/announcement/AnnouncementApplication.kt
@@ -6,12 +6,12 @@ import dsm.service.announcement.application.services.announcement.AnnouncementSe
 import dsm.service.announcement.domain.usecases.CreateAnnouncementUseCaseImpl
 import dsm.service.announcement.domain.usecases.GetAnnouncementUseCaseImpl
 import dsm.service.announcement.infrastructure.repositories.AnnouncementRepositoryImpl
-import dsm.service.announcement.presentation.services.announcement.AnnouncementServicer
+import dsm.service.announcement.presentation.servicers.AnnouncementServicer
 import io.grpc.ServerBuilder
 
 class AnnouncementApplication constructor(
-    private val port: Int,
-    private val announcementServicer: AnnouncementServicer
+    private val announcementServicer: AnnouncementServicer,
+    private val port: Int = 10000
 ) {
     val server = ServerBuilder
         .forPort(port)
@@ -39,24 +39,9 @@ class AnnouncementApplication constructor(
     }
 }
 
-fun createServicer(): AnnouncementServicer {
-    val announcementService = AnnouncementServiceImpl(
-        announcementMapper = AnnouncementMapper(),
-        getAnnouncementUseCase = GetAnnouncementUseCaseImpl(),
-        createAnnouncementUseCase = CreateAnnouncementUseCaseImpl(
-            announcementRepository = AnnouncementRepositoryImpl()
-        )
-    )
-
-    val announcementServicer = AnnouncementServicer(announcementService)
-
-    return announcementServicer
-}
 
 fun main() {
-    val port = 10000
-    val announcementServicer = createServicer()
-    val server = AnnouncementApplication(port, announcementServicer)
+    val server = AnnouncementApplication(AnnouncementServicer(), 10123)
 
     server.start()
     server.blockUntilShutdown()
