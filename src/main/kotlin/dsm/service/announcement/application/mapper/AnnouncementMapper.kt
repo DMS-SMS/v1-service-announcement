81523d48e960abe69b3c2bffce6b85dffd7a9eea
Maly <migsking@naver.com>
2020-09-24 16:42:15 +0900

    [CREATE] Announcement Mapper

---
+++ src/main/kotlin/dsm/service/announcement/application/mapper/AnnouncementMapper.kt
@@ -0,0 +1,21 @@
+package dsm.service.announcement.application.mapper
+
+import dsm.service.announcement.domain.entities.Announcement
+import dsm.service.announcement.proto.CreateAnnouncementRequest
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+class AnnouncementMapper {
+    fun createAnnouncementRequestMapper(
+        createAnnouncementRequest: CreateAnnouncementRequest,
+        contentUuid: String
+    ): Announcement {
+        return Announcement(
+            writerUuid = createAnnouncementRequest.uuid,
+            type = createAnnouncementRequest.type,
+            date = LocalDateTime.now(),
+            title = createAnnouncementRequest.title,
+            content = contentUuid
+        )
+    }
+}
