2cc739cbd50e7ad36da1f86def8d64febf851430
Maly <migsking@naver.com>
2020-09-24 17:50:43 +0900

    [ADD] 랜덤키 제네레이터 추가

--- src/main/kotlin/dsm/service/announcement/infrastructure/util/RandomKey.kt
+++ src/main/kotlin/dsm/service/announcement/infrastructure/util/RandomKey.kt
@@ -1,4 +1,15 @@
 package dsm.service.announcement.infrastructure.util
 
+import java.util.*
+import kotlin.streams.asSequence
+
+
 class RandomKey {
+    fun generateRandomKey(length: Long):String {
+        val source = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxtz123457890"
+        return Random().ints(length, 0, source.length)
+            .asSequence()
+            .map(source::get)
+            .joinToString("")
+    }
 }
