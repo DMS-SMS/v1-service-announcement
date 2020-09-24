a9af3682caf17fd1a690b1340c796871df78a8c2
Maly <migsking@naver.com>
2020-09-24 16:43:06 +0900

    [ADD] Announcement Service , create Announcement 추가

--- src/main/kotlin/dsm/service/announcement/application/services/announcement/AnnouncementService.kt
+++ src/main/kotlin/dsm/service/announcement/application/services/announcement/AnnouncementService.kt
@@ -1,8 +1,12 @@
 package dsm.service.announcement.application.services.announcement
 
+import dsm.service.announcement.proto.CreateAnnouncementRequest
+import dsm.service.announcement.proto.CreateAnnouncementResponse
 import dsm.service.announcement.proto.GetAnnouncementRequest
 import dsm.service.announcement.proto.GetAnnouncementResponse
 
 interface AnnouncementService {
     fun getAnnouncement(getAnnouncementRequest: GetAnnouncementRequest): GetAnnouncementResponse
+
+    fun createAnnouncement(createAnnouncementRequest: CreateAnnouncementRequest): CreateAnnouncementResponse
 }
