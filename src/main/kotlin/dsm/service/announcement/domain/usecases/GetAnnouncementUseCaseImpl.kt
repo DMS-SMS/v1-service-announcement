c03d072dce7d5a2385f89a47c4cd4ded9627666f
Maly <migsking@naver.com>
2020-09-24 16:44:37 +0900

    [CREATE] Get Announcement Use Case Implement

---
+++ src/main/kotlin/dsm/service/announcement/domain/usecases/GetAnnouncementUseCaseImpl.kt
@@ -0,0 +1,11 @@
+package dsm.service.announcement.domain.usecases
+
+class GetAnnouncementUseCaseImpl: GetAnnouncementUseCase {
+    override fun getClubAnnouncementUserCase() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getSchoolAnnouncementUserCase() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
