ee50b77bce20f87db840087f9fbb04cb7a73fc8e
Maly <migsking@naver.com>
2020-09-25 20:06:43 +0900

    [ADD] UseCase 추가

--- src/main/kotlin/dsm/service/announcement/domain/usecases/GetAnnouncementUseCaseImpl.kt
+++ src/main/kotlin/dsm/service/announcement/domain/usecases/GetAnnouncementUseCaseImpl.kt
@@ -2,15 +2,24 @@ package dsm.service.announcement.domain.usecases
 
 import dsm.service.announcement.domain.entities.Announcement
 import dsm.service.announcement.domain.repositories.AnnouncementRepository
+import org.bson.Document
 
 class GetAnnouncementUseCaseImpl(
     val announcementRepository: AnnouncementRepository
 ): GetAnnouncementUseCase {
-    override fun getAnnouncement(uuid: String, type: String): List<Announcement?> {
+    override fun getAnnouncements(uuid: String, type: String): List<Announcement?> {
         return if (type == "club") {
             announcementRepository.findClubAnnouncements()
         } else {
             announcementRepository.findSchoolAnnouncements(uuid)
         }
+    }
+
+    override fun getAnnouncement(uuid: String): Announcement? {
+        return announcementRepository.findByUuid(uuid)
+    }
+
+    override fun getAnnouncementContent(uuid: String): Document? {
+        return announcementRepository.findContentByUuid(uuid)
     }
 }
