a21766b117402d7f4c3a93c14ed9ebe19c168939
Maly <migsking@naver.com>
2020-09-25 20:09:30 +0900

    [MODIFY] 필드 추가

--- src/main/kotlin/dsm/service/announcement/application/mapper/AnnouncementMapper.kt
+++ src/main/kotlin/dsm/service/announcement/application/mapper/AnnouncementMapper.kt
@@ -3,7 +3,9 @@ package dsm.service.announcement.application.mapper
 import dsm.service.announcement.domain.entities.Announcement
 import dsm.service.announcement.proto.AnnouncementPreview
 import dsm.service.announcement.proto.CreateAnnouncementRequest
+import dsm.service.announcement.proto.GetAnnouncementDetailResponse
 import dsm.service.announcement.proto.GetAnnouncementResponse
+import org.bson.Document
 import java.sql.Timestamp
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -24,7 +26,7 @@ class AnnouncementMapper {
         )
     }
 
-    fun getAnnouncementResponseMapper(
+    fun getAnnouncementsResponseMapper(
         announcements: List<Announcement?>
     ): GetAnnouncementResponse {
         val getAnnouncementResponse = GetAnnouncementResponse.newBuilder()
@@ -32,21 +34,25 @@ class AnnouncementMapper {
 
         for(announcement in announcements) {
             if (announcement != null) {
-                    getAnnouncementResponse
-                        .addAnnouncement(
+                getAnnouncementResponse
+                    .addAnnouncement(
+                        announcement.number?.let {
                             AnnouncementPreview.newBuilder()
-                                .setNumber(1)
+                                .setNumber(it)
+                                .setAid(announcement.uuid)
                                 .setDate(
                                     Timestamp.valueOf(announcement.date).time)
                                 .setTitle(announcement.title)
                                 .setType(announcement.type)
                                 .setViews(1).build()
-                        )
-                }
+                        }
+                    )
             }
+        }
 
         return getAnnouncementResponse.build()
     }
+
     fun getAnnouncementResponseMapper(
         announcement: Announcement?,
         content: Document?
