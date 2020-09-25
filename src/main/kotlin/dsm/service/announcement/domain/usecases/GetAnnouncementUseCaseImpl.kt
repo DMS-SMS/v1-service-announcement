d100698c6d470b216f75f53558550cf7d8a09116
Maly <migsking@naver.com>
2020-09-25 16:00:16 +0900

    [ADD] Get Announcement Usecase

--- src/main/kotlin/dsm/service/announcement/domain/usecases/GetAnnouncementUseCaseImpl.kt
+++ src/main/kotlin/dsm/service/announcement/domain/usecases/GetAnnouncementUseCaseImpl.kt
@@ -1,9 +1,15 @@
 package dsm.service.announcement.domain.usecases
 
 import dsm.service.announcement.domain.entities.Announcement
+import dsm.service.announcement.domain.repositories.AnnouncementRepository
 
-class GetAnnouncementUseCaseImpl: GetAnnouncementUseCase {
-    override fun getAnnouncement(uuid: String, type: String): Announcement {
-        
+class GetAnnouncementUseCaseImpl(
+    val announcementRepository: AnnouncementRepository
+): GetAnnouncementUseCase {
+        return if (type == "club") {
+            announcementRepository.findClubAnnouncement()
+        } else {
+            announcementRepository.findSchoolAnnouncement(uuid)
+        }
     }
 }
