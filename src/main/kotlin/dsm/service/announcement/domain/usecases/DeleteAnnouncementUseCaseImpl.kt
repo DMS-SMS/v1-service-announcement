df052025c1e6ff1e846409ffaa6a894eff151412
Maly <migsking@naver.com>
2020-09-26 20:34:15 +0900

    [CREATE] DeleteAnnouncementUseCase

---
+++ src/main/kotlin/dsm/service/announcement/domain/usecases/DeleteAnnouncementUseCaseImpl.kt
@@ -0,0 +1,11 @@
+package dsm.service.announcement.domain.usecases
+
+class DeleteAnnouncementUseCaseImpl: DeleteAnnouncementUseCase {
+    override fun deleteAnnouncement(announcementUuid: String): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun deleteContent(contentUuid: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
