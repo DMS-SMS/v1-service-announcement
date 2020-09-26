1a96d7cd1f36bb2645e83379ac2c046e6fd99e55
Maly <migsking@naver.com>
2020-09-26 16:53:37 +0900

    [ADD] Update Service 추가

--- src/main/kotlin/dsm/service/announcement/application/services/announcement/AnnouncementServiceImpl.kt
+++ src/main/kotlin/dsm/service/announcement/application/services/announcement/AnnouncementServiceImpl.kt
@@ -4,10 +4,7 @@ import com.google.gson.JsonObject
 import dsm.service.announcement.application.mapper.AnnouncementMapper
 import dsm.service.announcement.domain.entities.Announcement
 import dsm.service.announcement.domain.repositories.AnnouncementRepository
-import dsm.service.announcement.domain.usecases.CreateAnnouncementUseCase
-import dsm.service.announcement.domain.usecases.CreateAnnouncementUseCaseImpl
-import dsm.service.announcement.domain.usecases.GetAnnouncementUseCase
-import dsm.service.announcement.domain.usecases.GetAnnouncementUseCaseImpl
+import dsm.service.announcement.domain.usecases.*
 import dsm.service.announcement.infrastructure.repositories.AnnouncementRepositoryImpl
 import dsm.service.announcement.proto.*
 import jdk.nashorn.internal.parser.JSONParser
@@ -16,7 +13,8 @@ import org.json.JSONObject
 class AnnouncementServiceImpl(
     val announcementMapper: AnnouncementMapper,
     val getAnnouncementUseCase: GetAnnouncementUseCase,
-    val createAnnouncementUseCase: CreateAnnouncementUseCase
+    val createAnnouncementUseCase: CreateAnnouncementUseCase,
+    val updateAnnouncementUseCase: UpdateAnnouncementUseCase
 ) : AnnouncementService {
     override fun getAnnouncement(getAnnouncementRequest: GetAnnouncementRequest): GetAnnouncementResponse {
         val announcements: List<Announcement?> = getAnnouncementUseCase.getAnnouncements(
@@ -45,5 +43,25 @@ class AnnouncementServiceImpl(
 
         return CreateAnnouncementResponse.newBuilder()
             .build()
+    }
+
+    override fun updateAnnouncement(updateAnnouncementRequest: UpdateAnnouncementRequest): UpdateAnnouncementResponse {
+        val contentUuid = updateAnnouncementUseCase.updateAnnouncement(
+            updateAnnouncementRequest.aid,
+            updateAnnouncementRequest.title,
+            updateAnnouncementRequest.targetGrade,
+            updateAnnouncementRequest.targetClass)
+
+        if (contentUuid != null) {
+            updateAnnouncementUseCase.updateContent(
+                contentUuid,
+                updateAnnouncementRequest.content)
+        }
+
+        return UpdateAnnouncementResponse.newBuilder().build()
+    }
+
+    override fun deleteAnnouncement(deleteAnnouncementRequest: DeleteAnnouncementRequest): DeleteAnnouncementResponse {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
