e429a7069ba06d070ca3b5210123e085b730e519
Maly <migsking@naver.com>
2020-09-27 15:47:19 +0900

    [ADD] AuthorityFailedException 추가

--- src/main/kotlin/dsm/service/announcement/domain/exceptions/AuthorityFailedException.kt
+++ src/main/kotlin/dsm/service/announcement/domain/exceptions/AuthorityFailedException.kt
@@ -1,4 +1,8 @@
 package dsm.service.announcement.domain.exceptions
 
-class AuthorityFailedException {
+class AuthorityFailedException: BusinessException {
+    constructor(status: Int = 401,
+                code: Int = -1001,
+                message: String = "Authority Error"):
+            super(status, code, message)
 }
