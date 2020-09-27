5202397b6ccde04625960bc590f6f9eaf90de9e7
Maly <migsking@naver.com>
2020-09-27 15:52:47 +0900

    [CREATE] Not Found Exception 추가

--- src/main/kotlin/dsm/service/announcement/domain/exceptions/NotFoundException.kt
+++ src/main/kotlin/dsm/service/announcement/domain/exceptions/NotFoundException.kt
@@ -1,4 +1,9 @@
 package dsm.service.announcement.domain.exceptions
 
-class NotFoundException {
+class NotFoundException: BusinessException {
+    constructor(status: Int = 404,
+                code: Int = -1001,
+                message: String = "Not Found"):
+            super(status, code, message)
+} {
 }
