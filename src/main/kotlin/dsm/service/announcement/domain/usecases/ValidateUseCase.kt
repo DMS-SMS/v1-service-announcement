c061b8ed526df0df5909530a96eacff11a063464
Maly <migsking@naver.com>
2020-09-27 17:24:34 +0900

    [ADD] Validate Auth 추가

--- src/main/kotlin/dsm/service/announcement/domain/usecases/ValidateUseCase.kt
+++ src/main/kotlin/dsm/service/announcement/domain/usecases/ValidateUseCase.kt
@@ -2,4 +2,6 @@ package dsm.service.announcement.domain.usecases
 
 interface ValidateUseCase {
     fun validateAnnouncementAuthority(announcementUuid: String, userUuid: String)
+
+    fun validateAuth(uuid: String, option: String)
 }
