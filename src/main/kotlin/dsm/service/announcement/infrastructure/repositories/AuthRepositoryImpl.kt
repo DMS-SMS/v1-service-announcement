e424b17a1e5aa9c8fa3b16e26088906109e5a2b5
Maly <migsking@naver.com>
2020-09-27 17:23:28 +0900

    [ADD] 임시 구현체 추가

--- src/main/kotlin/dsm/service/announcement/infrastructure/repositories/AuthRepositoryImpl.kt
+++ src/main/kotlin/dsm/service/announcement/infrastructure/repositories/AuthRepositoryImpl.kt
@@ -1,4 +1,11 @@
 package dsm.service.announcement.infrastructure.repositories
 
-class AuthRepositoryImpl {
+import dsm.service.announcement.domain.exceptions.AuthorityFailedException
+import dsm.service.announcement.domain.repositories.AuthRepository
+
+
+class AuthRepositoryImpl: AuthRepository {
+    override fun validateAuth(uuid: String, option: String) {
+        if (uuid == "") throw AuthorityFailedException()
+    }
 }
