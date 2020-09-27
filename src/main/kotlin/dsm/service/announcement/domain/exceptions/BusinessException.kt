b2005bfc0faa86350ad2ca163b4ad37997f67bed
Maly <migsking@naver.com>
2020-09-27 15:45:02 +0900

    [CREATE] BusinessException

--- src/main/kotlin/dsm/service/announcement/domain/exceptions/BusinessException.kt
+++ src/main/kotlin/dsm/service/announcement/domain/exceptions/BusinessException.kt
@@ -1,4 +1,9 @@
 package dsm.service.announcement.domain.exceptions
 
-interface BusinessException {
-}
+import java.lang.RuntimeException
+
+open class BusinessException(
+    val status: Int,
+    val code: Int,
+    message: String
+): RuntimeException(message)
