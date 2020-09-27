dc8dd647c843c2011f28aaac0a408663dd710e4d
Maly <migsking@naver.com>
2020-09-27 17:24:10 +0900

    [ADD] Validate UseCase 추가

--- src/main/kotlin/dsm/service/announcement/domain/usecases/ValidateUseCaseImpl.kt
+++ src/main/kotlin/dsm/service/announcement/domain/usecases/ValidateUseCaseImpl.kt
@@ -1,11 +1,17 @@
 package dsm.service.announcement.domain.usecases
 
 import dsm.service.announcement.domain.repositories.AnnouncementRepository
+import dsm.service.announcement.domain.repositories.AuthRepository
 
 class ValidateUseCaseImpl(
-    val announcementRepository: AnnouncementRepository
+    val announcementRepository: AnnouncementRepository,
+    val authRepository: AuthRepository
 ): ValidateUseCase {
     override fun validateAnnouncementAuthority(announcementUuid: String, userUuid: String) {
-        val announcement = announcementRepository.validateAnnouncement(announcementUuid, userUuid)
+        announcementRepository.validateAnnouncement(announcementUuid, userUuid)
+    }
+
+    override fun validateAuth(uuid: String, option: String) {
+        authRepository.validateAuth(uuid, option)
     }
 }
