be2f4286b4483fcf44b2296da6911b3f8853f5d4
Maly <migsking@naver.com>
2020-10-12 20:17:09 +0900

    [DELETE] <*>

--- src/main/kotlin/dsm/service/announcement/domain/exceptions/BusinessException.kt
+++
@@ -1,9 +0,0 @@
-package dsm.service.announcement.domain.exceptions
-
-import java.lang.RuntimeException
-
-open class BusinessException(
-    val status: Int,
-    val code: Int,
-    message: String
-): RuntimeException(message)
--- src/main/kotlin/dsm/service/announcement/domain/exceptions/NotFoundException.kt
+++
@@ -1,8 +0,0 @@
-package dsm.service.announcement.domain.exceptions
-
-class NotFoundException: BusinessException {
-    constructor(status: Int = 404,
-                code: Int = -1001,
-                message: String = "Not Found"):
-            super(status, code, message)
-}
