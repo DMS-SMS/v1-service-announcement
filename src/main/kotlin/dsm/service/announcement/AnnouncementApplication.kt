155039a72369eca06daae16f49b9bf0cf8b5368d
Maly <migsking@naver.com>
2020-10-12 20:19:43 +0900

    [CREATE] #11 Spring Init

---
+++ src/main/kotlin/dsm/service/announcement/AnnouncementApplication.kt
@@ -0,0 +1,11 @@
+package dsm.service.announcement
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AnnouncementApplication
+
+fun main(args: Array<String>) {
+	runApplication<AnnouncementApplication>(*args)
+}
---
+++ src/test/kotlin/dsm/service/announcement/AnnouncementApplicationTests.kt
@@ -0,0 +1,13 @@
+package dsm.service.announcement
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AnnouncementApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
