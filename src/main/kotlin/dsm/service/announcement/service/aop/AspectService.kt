71a47dc237842c5899e5eacd054cf92635955f1d
Maly <migsking@naver.com>
2020-10-14 14:36:06 +0900

    [CREATE] #14 Aspect 작성

---
+++ src/main/kotlin/dsm/service/announcement/service/aop/AspectService.kt
@@ -0,0 +1,26 @@
+package dsm.service.announcement.service.aop
+
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
+import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.annotation.Pointcut
+import org.springframework.stereotype.Component
+import java.lang.Exception
+
+
+@Component
+@Aspect
+public class AspectService {
+    @Pointcut("execution(* dsm.service.announcement.service.AnnouncementServiceImpl.*(..))")
+    fun tracingPointCut() {}
+
+    @Around("tracingPointCut()")
+    fun ErrorHandling(pjp: ProceedingJoinPoint): Any {
+        try {
+            return pjp.proceed()
+        } catch (e: Exception) {
+            println(e)
+        }
+        return pjp.proceed()
+    }
+}
