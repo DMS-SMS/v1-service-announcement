42d08145495e956f8e4ce60b9019e08dea2a1c1a
Maly <migsking@naver.com>
2020-10-18 00:11:26 +0900

    [MODIFY] #7 MongoDB Consul KV 연결

--- src/main/kotlin/dsm/service/announcement/infra/mongodb/MongoDBConfiguration.kt
+++ src/main/kotlin/dsm/service/announcement/infra/mongodb/MongoDBConfiguration.kt
@@ -3,9 +3,10 @@ package dsm.service.announcement.infra.mongodb
 import com.mongodb.MongoClientSettings
 import com.mongodb.client.MongoClient
 import com.mongodb.client.MongoClients
+import dsm.service.announcement.infra.consul.ConsulHandler
+import org.json.simple.JSONObject
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.mongodb.MongoDatabaseFactory
-import org.springframework.data.mongodb.MongoDbFactory
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration
 import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver
@@ -16,7 +17,10 @@ import org.springframework.data.mongodb.core.mapping.MongoMappingContext
 
 @Configuration
 class MongoDBConfiguration(
+        private final val consulHandler: ConsulHandler
 ): AbstractMongoClientConfiguration() {
+    val mongoConfig: JSONObject = consulHandler.getValue("mongo/announcement/local")
+
     override fun mappingMongoConverter(databaseFactory: MongoDatabaseFactory, customConversions: MongoCustomConversions, mappingContext: MongoMappingContext): MappingMongoConverter {
         val dbRefResolver = DefaultDbRefResolver(databaseFactory)
         val mappingMongoConverter = MappingMongoConverter(dbRefResolver, mappingContext)
@@ -26,10 +30,15 @@ class MongoDBConfiguration(
     }
 
     override fun mongoClient(): MongoClient {
-        return MongoClients.create("mongodb://localhost:27017/javatest")
+        mongoConfig["url"]?.let {
+            return MongoClients.create(it as String)
+        }
+        return MongoClients.create("")
     }
 
     override fun getDatabaseName(): String {
-        return "javatest"
+        mongoConfig["db"]?.let {
+            return it as String }
+        return ""
     }
 }
