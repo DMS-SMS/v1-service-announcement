c7014559b6ac36acf31aefd496c20fa0501ab614
Maly <migsking@naver.com>
2020-10-18 00:09:55 +0900

    [CREATE] #8 Student Repo 구현

---
+++ src/main/kotlin/dsm/service/announcement/infra/implementation/repository/StudentRepositoryImpl.kt
@@ -0,0 +1,21 @@
+package dsm.service.announcement.infra.implementation.repository
+
+import dsm.service.announcement.domain.entity.Account
+import dsm.service.announcement.domain.repository.StudentRepository
+import dsm.service.announcement.infra.auth.AuthHandler
+import dsm.service.announcement.infra.auth.mapper.AuthMapper
+import org.springframework.stereotype.Repository
+import kotlinx.coroutines.runBlocking
+
+
+@Repository
+class StudentRepositoryImpl(
+        val authHandler: AuthHandler,
+        val authMapper: AuthMapper
+): StudentRepository {
+    override fun findByUuid(uuid: String, xRequestId: String): Account? = runBlocking {
+        return@runBlocking authMapper.getStudentInformWithUuidMapper(
+                authHandler.getStudentInform(uuid, xRequestId)
+        )
+    }
+}
