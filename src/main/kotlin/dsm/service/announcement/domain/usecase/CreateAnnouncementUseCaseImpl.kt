36a6a83f2ba0feefa4dda51041691994b774dc8e
Maly <migsking@naver.com>
2020-10-19 20:00:50 +0900

    [ADD] #4 CreateAnnouncement Logic

--- src/main/kotlin/dsm/service/announcement/domain/usecase/CreateAnnouncementUseCaseImpl.kt
+++ src/main/kotlin/dsm/service/announcement/domain/usecase/CreateAnnouncementUseCaseImpl.kt
@@ -3,9 +3,9 @@ package dsm.service.announcement.domain.usecase
 import com.mongodb.BasicDBObject
 import dsm.service.announcement.domain.entity.Announcement
 import dsm.service.announcement.domain.entity.Content
-import dsm.service.announcement.domain.repository.AnnouncementRepository
-import dsm.service.announcement.domain.repository.ContentRepository
-import dsm.service.announcement.domain.repository.TeacherRepository
+import dsm.service.announcement.domain.exception.NotFoundException
+import dsm.service.announcement.domain.exception.UnAuthorizedException
+import dsm.service.announcement.domain.repository.*
 import dsm.service.announcement.domain.service.UuidService
 import org.springframework.data.annotation.Id
 import org.springframework.stereotype.Component
@@ -17,12 +17,32 @@ import javax.persistence.GenerationType
 @Component
 class CreateAnnouncementUseCaseImpl(
         val announcementRepository: AnnouncementRepository,
+        val clubRepository: ClubRepository,
+        val teacherRepository: TeacherRepository,
         val contentRepository: ContentRepository,
         val uuidService: UuidService
 ): CreateAnnouncementUseCase {
-    override fun run(writerUuid: String, title: String, content: String, targetGrade: Int, targetGroup: Int, type: String){
-        // TODO 동아리장 또는 선생 인증 관련 로직 추가
-        println("ASDFASDF")
+    override fun run(
+            writerUuid: String,
+            title: String,
+            content: String,
+            targetGrade: Int,
+            targetGroup: Int,
+            type: String,
+            xRequestId: String
+    ) {
+        var club: String? = null
+        teacherRepository.findByUuid(writerUuid, xRequestId)?.let {
+            if (it.grade == 0 || it.group == 0) {
+                val clubUuid = clubRepository.findClubUuidByLeaderUuid(writerUuid, xRequestId)
+                clubUuid.let {
+                    if (it.isEmpty()) throw UnAuthorizedException()
+                    if (type == "school") throw UnAuthorizedException() // UnAuth
+                }
+
+                club = clubRepository.findByUuid(clubUuid, writerUuid, xRequestId).name
+            }
+        }
 
         val announcementUuid = uuidService.createAnnouncementUuid()
 
@@ -34,7 +54,8 @@ class CreateAnnouncementUseCaseImpl(
                     title=title,
                     targetGrade=targetGrade,
                     targetGroup=targetGroup,
-                    type=type
+                    type=type,
+                    club=club
             )
         )
 
