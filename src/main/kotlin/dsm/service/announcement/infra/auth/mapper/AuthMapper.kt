28b0997fe722e2571a2084fe466734c83d31cf0d
Maly <migsking@naver.com>
2020-10-19 20:01:29 +0900

    [ADD] #8 Auth 서비스 학부모용 매퍼 추가

--- src/main/kotlin/dsm/service/announcement/infra/auth/mapper/AuthMapper.kt
+++ src/main/kotlin/dsm/service/announcement/infra/auth/mapper/AuthMapper.kt
@@ -2,11 +2,16 @@ package dsm.service.announcement.infra.auth.mapper
 
 import dsm.service.announcement.domain.entity.Account
 import dsm.service.announcement.proto.GetStudentInformWithUUIDResponse
+import dsm.service.announcement.proto.GetTeacherInformWithUUIDResponse
 import org.springframework.stereotype.Component
 
 @Component
 class AuthMapper {
     fun getStudentInformWithUuidMapper(response: GetStudentInformWithUUIDResponse): Account {
+        return Account(response.grade, response.group, response.name, response.phoneNumber)
+    }
+
+    fun getTeacherInformWithUuidMapper(response: GetTeacherInformWithUUIDResponse): Account {
         return Account(response.grade, response.group, response.name, response.phoneNumber)
     }
 }
