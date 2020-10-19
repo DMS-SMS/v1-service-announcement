69c61ba4a40db0f4242da49cd089becd5aecba80
Maly <migsking@naver.com>
2020-10-19 19:59:52 +0900

    [MODIDFY] #8 xRequestId 받음

--- src/main/kotlin/dsm/service/announcement/domain/repository/TeacherRepository.kt
+++ src/main/kotlin/dsm/service/announcement/domain/repository/TeacherRepository.kt
@@ -3,5 +3,5 @@ package dsm.service.announcement.domain.repository
 import dsm.service.announcement.domain.entity.Account
 
 interface TeacherRepository {
-    fun findById(uuid: String): Account?;
+    fun findByUuid(uuid: String, xRequestId: String): Account?
 }
