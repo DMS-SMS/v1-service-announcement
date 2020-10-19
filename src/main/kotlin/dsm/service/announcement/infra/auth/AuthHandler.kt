f3d555174f18c291640b7762d6b4e4bccf711816
Maly <migsking@naver.com>
2020-10-19 20:01:18 +0900

    [ADD] #8 Auth 서비스 연결

--- src/main/kotlin/dsm/service/announcement/infra/auth/AuthHandler.kt
+++ src/main/kotlin/dsm/service/announcement/infra/auth/AuthHandler.kt
@@ -2,9 +2,7 @@ package dsm.service.announcement.infra.auth
 
 import dsm.service.announcement.infra.consul.ConsulHandler
 import dsm.service.announcement.infra.jaeger.JaegerHandler
-import dsm.service.announcement.proto.AuthStudentGrpcKt
-import dsm.service.announcement.proto.GetStudentInformWithUUIDRequest
-import dsm.service.announcement.proto.GetStudentInformWithUUIDResponse
+import dsm.service.announcement.proto.*
 import dsm.service.announcement.service.aop.annotation.Tracing
 import io.grpc.ManagedChannel
 import io.grpc.ManagedChannelBuilder
@@ -50,6 +48,36 @@ class AuthHandler(
             MetadataUtils.attachHeaders(stub, metadata).getStudentInformWithUUID(request)
         } catch (e: Exception) {
             GetStudentInformWithUUIDResponse.newBuilder().build()
+        }
+    }
+
+    suspend fun getTeacherInform(
+            uuid: String,
+            xRequestId: String
+    ): GetTeacherInformWithUUIDResponse {
+        val channel: ManagedChannel = ManagedChannelBuilder.forAddress(
+//                consulHandler.getServiceHost(serviceName),
+                host,
+                consulHandler.getServicePort(serviceName)
+        ).usePlaintext().build()
+        val stub: AuthTeacherGrpcKt.AuthTeacherCoroutineStub = AuthTeacherGrpcKt.AuthTeacherCoroutineStub(channel)
+
+
+        val metadata: Metadata = Metadata()
+        val spanContext = jaegerHandler.getActiveSpanString()
+
+        metadata.put(Metadata.Key.of("x-request-id", Metadata.ASCII_STRING_MARSHALLER), xRequestId)
+        metadata.put(Metadata.Key.of("span-context", Metadata.ASCII_STRING_MARSHALLER), spanContext)
+
+        val request = GetTeacherInformWithUUIDRequest.newBuilder()
+                .setUUID(uuid)
+                .setTeacherUUID(uuid)
+                .build()
+
+        return try {
+            MetadataUtils.attachHeaders(stub, metadata).getTeacherInformWithUUID(request)
+        } catch (e: Exception) {
+            GetTeacherInformWithUUIDResponse.newBuilder().build()
         }
     }
 }
