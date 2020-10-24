0730e2c088d0e52cc0437158907796f75babbbe3
Maly <migsking@naver.com>
2020-10-25 00:14:14 +0900

    [ADD] #4 next, previous 추가

--- src/main/kotlin/dsm/service/announcement/service/mapper/AnnouncementMapper.kt
+++ src/main/kotlin/dsm/service/announcement/service/mapper/AnnouncementMapper.kt
@@ -2,6 +2,7 @@ package dsm.service.announcement.service.mapper
 
 import dsm.service.announcement.domain.entity.Announcement
 import dsm.service.announcement.domain.entity.Content
+import dsm.service.announcement.domain.repository.ViewRepository
 import dsm.service.announcement.proto.AnnouncementPreview
 import dsm.service.announcement.proto.GetAnnouncementDetailResponse
 import dsm.service.announcement.proto.GetAnnouncementsResponse
@@ -9,17 +10,21 @@ import org.springframework.stereotype.Component
 import java.sql.Timestamp
 
 @Component
-public class AnnouncementMapper {
+public class AnnouncementMapper(
+        val viewRepository: ViewRepository
+) {
     fun getAnnouncementsMapper(announcements: MutableIterable<Announcement>): GetAnnouncementsResponse.Builder {
         val response = GetAnnouncementsResponse.newBuilder();
 
         for(announcement: Announcement in announcements) {
             val previewBuilder = AnnouncementPreview.newBuilder()
-            previewBuilder
-                    .setAnnouncementId(announcement.uuid)
-                    .setTitle(announcement.title)
-                    .setDate(Timestamp.valueOf(announcement.date).time)
-                    .setViews(0)
+            viewRepository.findByUuid(announcement.uuid)?.read_accounts?.size?.toLong()?.let {
+                previewBuilder
+                        .setAnnouncementId(announcement.uuid)
+                        .setTitle(announcement.title)
+                        .setDate(Timestamp.valueOf(announcement.date).time)
+                        .setViews(it)
+            }
 
             announcement.number?.let {
                 previewBuilder.setNumber(it)
@@ -35,11 +40,23 @@ public class AnnouncementMapper {
         return response
     }
 
-    fun getAnnouncementDetailMapper(announcement: Announcement, content: String): GetAnnouncementDetailResponse.Builder {
+    fun getAnnouncementDetailMapper(
+            announcement: Announcement, content: String, nextAnnouncement: Announcement?, previousAnnouncement: Announcement?
+    ): GetAnnouncementDetailResponse.Builder {
         val response = GetAnnouncementDetailResponse.newBuilder();
         response.title = announcement.title
         response.date = Timestamp.valueOf(announcement.date).time
         response.content = content
+
+        nextAnnouncement?.let {
+            response.nextAnnouncementId = nextAnnouncement.uuid
+            response.nextTitle = nextAnnouncement.title
+        }
+
+        previousAnnouncement?.let {
+            response.previousAnnouncementId = previousAnnouncement.uuid
+            response.previousTitle = previousAnnouncement.title
+        }
         return response
     }
 }
