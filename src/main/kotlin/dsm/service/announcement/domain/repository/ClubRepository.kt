7a386df2b271a89c9ae30e4ae6a5bb4739eb1fc3
Maly <migsking@naver.com>
2020-10-25 00:12:22 +0900

    [MODIFY] #2 Null 처리

--- src/main/kotlin/dsm/service/announcement/domain/repository/ClubRepository.kt
+++ src/main/kotlin/dsm/service/announcement/domain/repository/ClubRepository.kt
@@ -3,7 +3,7 @@ package dsm.service.announcement.domain.repository
 import dsm.service.announcement.domain.entity.Club
 
 interface ClubRepository {
-    fun findClubUuidByLeaderUuid(leaderUuid: String): String
+    fun findClubUuidByLeaderUuid(leaderUuid: String): String?
 
-    fun findByUuid(clubUuid: String, accountUuid: String): Club
+    fun findByUuid(clubUuid: String, accountUuid: String): Club?
 }
