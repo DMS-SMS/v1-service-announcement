fb67c0a326cbeb01d9c3f52f03a33ab491b9ce1f
Maly <migsking@naver.com>
2020-12-12 00:26:18 +0900

    [MODIFY] Mapper

--- src/main/kotlin/dsm/service/announcement/service/mapper/AnnouncementMapper.kt
+++ src/main/kotlin/dsm/service/announcement/service/mapper/AnnouncementMapper.kt
@@ -3,6 +3,7 @@ package dsm.service.announcement.service.mapper
 import dsm.service.announcement.domain.entity.Announcement
 import dsm.service.announcement.domain.entity.Content
 import dsm.service.announcement.domain.repository.ViewRepository
+import dsm.service.announcement.domain.usecase.GetAccountUseCase
 import dsm.service.announcement.proto.AnnouncementPreview
 import dsm.service.announcement.proto.GetAnnouncementDetailResponse
 import dsm.service.announcement.proto.GetAnnouncementsResponse
@@ -11,19 +12,25 @@ import java.sql.Timestamp
 
 @Component
 public class AnnouncementMapper(
-        val viewRepository: ViewRepository
+        val viewRepository: ViewRepository,
+        val getAccountUseCase: GetAccountUseCase
 ) {
     fun getAnnouncementsMapper(announcements: MutableIterable<Announcement>): GetAnnouncementsResponse.Builder {
         val response = GetAnnouncementsResponse.newBuilder();
 
         for(announcement: Announcement in announcements) {
             val previewBuilder = AnnouncementPreview.newBuilder()
-            viewRepository.findByUuid(announcement.uuid)?.read_accounts?.size?.toLong()?.let {
+            viewRepository.findByUuid(announcement.uuid)?.read_accounts?.let {
+                var checked = 0
+                it.find { it == announcement.writerUuid }.let {
+                    checked = 1
+                }
                 previewBuilder
                         .setAnnouncementId(announcement.uuid)
                         .setTitle(announcement.title)
                         .setDate(Timestamp.valueOf(announcement.date).time)
-                        .setViews(it)
+                        .setViews(it.size.toLong())
+                        .setIsChecked(checked.toLong())
             }
 
             announcement.number?.let {
@@ -31,8 +38,10 @@ public class AnnouncementMapper(
             }
 
             announcement.club?.let {
-                previewBuilder.setClub(it)
-            }
+                previewBuilder.setWriterName(it)
+            }?.let {
+                val userName: String = getAccountUseCase.execute(announcement.writerUuid)?.name ?: ""
+                previewBuilder.setWriterName(userName) }
 
             response.addAnnouncement(previewBuilder.build())
         }
@@ -47,6 +56,12 @@ public class AnnouncementMapper(
         response.title = announcement.title
         response.date = Timestamp.valueOf(announcement.date).time
         response.content = content
+
+        getAccountUseCase.execute(announcement.writerUuid).let {
+            if (it != null) {
+                response.writerName = it.name
+            }
+        }
 
         nextAnnouncement?.let {
             response.nextAnnouncementId = nextAnnouncement.uuid
