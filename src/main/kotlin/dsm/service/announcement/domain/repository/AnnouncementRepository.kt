ff1b547edc0ef456b8f11511ab9887a1e3b1982f
Maly <migsking@naver.com>
2020-12-13 10:40:31 +0900

    [ADD] #87 Repository

--- src/main/kotlin/dsm/service/announcement/domain/repository/AnnouncementRepository.kt
+++ src/main/kotlin/dsm/service/announcement/domain/repository/AnnouncementRepository.kt
@@ -1,6 +1,8 @@
 package dsm.service.announcement.domain.repository
 
 import dsm.service.announcement.domain.entity.Announcement
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.CrudRepository
 
 interface AnnouncementRepository: CrudRepository<Announcement, Long> {
@@ -12,7 +14,9 @@ interface AnnouncementRepository: CrudRepository<Announcement, Long> {
 
     fun findAllByType(type: String): MutableIterable<Announcement>
 
+    fun findAllByType(type: String, pageable: Pageable): Page<Announcement>
+
     fun findByNumber(number: Long): Announcement?
 
-    fun findAllByTypeAndTargetGradeAndTargetGroup(type: String, grade: Int, group: Int): MutableIterable<Announcement>
+    fun findAllByTypeAndTargetGradeAndTargetGroup(type: String, grade: Int, group: Int, pageable: Pageable): Page<Announcement>
 }
