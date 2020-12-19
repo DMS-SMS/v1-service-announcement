54d61c32f57ce148a2e69b055a78456192c1d31a
Maly <migsking@naver.com>
2020-12-19 16:41:52 +0900

    [MODIFY] Check

--- src/main/kotlin/dsm/service/announcement/service/mapper/AnnouncementMapper.kt
+++ src/main/kotlin/dsm/service/announcement/service/mapper/AnnouncementMapper.kt
@@ -26,12 +26,12 @@ public class AnnouncementMapper(
                     .setDate(Timestamp.valueOf(announcement.date).time)
                     .setIsChecked(0)
             viewRepository.findByUuid(announcement.uuid).let {
+
                 it?.read_accounts?.size?.toLong()?.let { size ->
                     previewBuilder.setViews(size)
                 }
                 it?.read_accounts?.find { it == announcement.writerUuid }.let {
-                    previewBuilder
-                            .setIsChecked(1)
+                    if (it != null) previewBuilder.setIsChecked(1)
                 }
             }
 
