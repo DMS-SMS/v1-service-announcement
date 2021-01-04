0f172b314656359e6815e37d7a9cdbf6706c84fc
kangsinhee <rkdtlsgml40@naver.com>
2021-01-04 22:19:13 +0900

    [ADD] Get PreviousAnnouncement UseCase

--- src/main/kotlin/dsm/service/announcement/core/domain/repository/AnnouncementRepository.kt
+++ src/main/kotlin/dsm/service/announcement/core/domain/repository/AnnouncementRepository.kt
@@ -6,9 +6,10 @@ interface AnnouncementRepository {
     fun persist(announcement: Announcement): Announcement
     fun delete(announcement: Announcement)
 
-    fun findById(id: String): Announcement?;
+    fun findById(id: String): Announcement?
 
-    fun findByNumberAndType(number: Long, type: String): Announcement?;
+    fun findByNumberAndType(number: Long, type: String): Announcement?
 
-    fun findTopByOrderByNumberDesc(): Announcement?;
+    fun findTopByOrderByNumberDesc(): Announcement?
+    fun findTopByOrderByNumberAsc(): Announcement?
 }
