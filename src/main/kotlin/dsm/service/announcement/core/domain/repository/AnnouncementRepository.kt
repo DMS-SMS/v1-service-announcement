628c3d5c1ac092809d5cc9aa302dcad3b39417b0
Maly <migsking@naver.com>
2021-01-04 23:22:21 +0900

    [MODIFY] Announcement Repository

--- src/main/kotlin/dsm/service/announcement/core/domain/repository/AnnouncementRepository.kt
+++ src/main/kotlin/dsm/service/announcement/core/domain/repository/AnnouncementRepository.kt
@@ -1,14 +1,17 @@
 package dsm.service.announcement.core.domain.repository
 
 import dsm.service.announcement.core.domain.entity.Announcement
+import org.springframework.stereotype.Repository
 
+@Repository
 interface AnnouncementRepository {
     fun persist(announcement: Announcement): Announcement
+
     fun delete(announcement: Announcement)
 
-    fun findById(id: String): Announcement?;
+    fun findById(id: String): Announcement?
 
-    fun findByNumberAndType(number: Long, type: String): Announcement?;
+    fun findByNumberAndType(number: Long, type: String): Announcement?
 
-    fun findTopByOrderByNumberDesc(): Announcement?;
+    fun findTopByOrderByNumberDesc(): Announcement?
 }
