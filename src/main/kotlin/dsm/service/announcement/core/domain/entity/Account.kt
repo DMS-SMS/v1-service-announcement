e76115640104e9ef3f396f3417614af1f39f2d42
Maly <migsking@naver.com>
2021-01-04 19:20:33 +0900

    [ADD] field in account

--- src/main/kotlin/dsm/service/announcement/core/domain/entity/Account.kt
+++ src/main/kotlin/dsm/service/announcement/core/domain/entity/Account.kt
@@ -1,8 +1,11 @@
 package dsm.service.announcement.core.domain.entity
 
+import dsm.service.announcement.core.domain.entity.enums.AccountType
+
 data class Account(
         val grade: Int,
         val group: Int,
         val name: String,
-        val phoneNumber: String
+        val phoneNumber: String,
+        val type: AccountType
 )
