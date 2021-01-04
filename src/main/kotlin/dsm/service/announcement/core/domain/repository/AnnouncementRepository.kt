2ca89c102b20c731770bef99efd952d1d51e059c
kangsinhee <rkdtlsgml40@naver.com>
2021-01-04 18:49:18 +0900

    [ADD] DeleteAnnouncementUseCase

--- src/main/kotlin/dsm/service/announcement/core/domain/repository/AnnouncementRepository.kt
+++ src/main/kotlin/dsm/service/announcement/core/domain/repository/AnnouncementRepository.kt
@@ -3,7 +3,8 @@ package dsm.service.announcement.core.domain.repository
 import dsm.service.announcement.core.domain.entity.Announcement
 
 interface AnnouncementRepository {
-    fun persist(announcement: Announcement): Announcement;
+    fun persist(announcement: Announcement): Announcement
+    fun delete(announcement: Announcement)
 
-    fun findById(id: String): Announcement?;
+    fun findById(id: String): Announcement?
 }
