f0d30088132e5b2fb4b10fe1f1ffb522de74a5aa
Maly <migsking@naver.com>
2021-01-04 23:14:14 +0900

    [MODIFY] Executor

--- src/main/kotlin/dsm/service/announcement/core/usecase/UseCaseExecutor.kt
+++ src/main/kotlin/dsm/service/announcement/core/usecase/UseCaseExecutor.kt
@@ -1,12 +1,12 @@
 package dsm.service.announcement.core.usecase
 
+import dsm.service.announcement.controller.mapper.Mapper
 import dsm.service.announcement.core.usecase.UseCase.InputValues
 import dsm.service.announcement.core.usecase.UseCase.OutputValues
-import java.util.concurrent.CompletableFuture
 import java.util.function.Function
 
 
 interface UseCaseExecutor {
-    fun <RX, I : InputValues, O : OutputValues> execute(
-            useCase: UseCase<I, O>, input: I, outputMapper: Function<O, RX>): CompletableFuture<RX>
+    fun <I : InputValues, O : OutputValues, Res> execute(
+            useCase: UseCase<I, O>, input: I, outputMapper: Mapper<O, Res>): Res
 }
