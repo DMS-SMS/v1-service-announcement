6300bc426f9f788f94a9f6d0e99e1e2b884e47a6
kangsinhee <rkdtlsgml40@naver.com>
2021-01-05 09:49:50 +0900

    [ADD] Search Announcements UseCase

--- src/main/kotlin/dsm/service/announcement/core/domain/repository/AnnouncementRepository.kt
+++ src/main/kotlin/dsm/service/announcement/core/domain/repository/AnnouncementRepository.kt
@@ -1,6 +1,8 @@
 package dsm.service.announcement.core.domain.repository
 
 import dsm.service.announcement.core.domain.entity.Announcement
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 
 interface AnnouncementRepository {
     fun persist(announcement: Announcement): Announcement
@@ -11,5 +13,13 @@ interface AnnouncementRepository {
     fun findByNumberAndType(number: Long, type: String): Announcement?
 
     fun findTopByOrderByNumberDesc(): Announcement?
+
     fun findTopByOrderByNumberAsc(): Announcement?
+
+    fun findByTitleContainsAndTypeOrderByDateDesc(title: String, type: String, pageable: Pageable): Page<Announcement>
+
+    fun countByTitleContainsAndType(title: String, type: String): Long
+
+    fun findByTitleContainsAndTypeAndTargetGradeContainsAndTargetGroupContainsOrderByDateDesc(
+            title: String, type: String, targetGrade: String, targetGroup: String, pageable: Pageable): Page<Announcement>
 }
--- src/main/kotlin/dsm/service/announcement/core/usecase/announcement/SearchAnnouncementsUseCase.kt
+++ src/main/kotlin/dsm/service/announcement/core/usecase/announcement/SearchAnnouncementsUseCase.kt
@@ -1,2 +1,59 @@
 package dsm.service.announcement.core.usecase.announcement
 
+import dsm.service.announcement.core.domain.entity.Announcement
+import dsm.service.announcement.core.domain.exception.BadRequestException
+import dsm.service.announcement.core.domain.repository.AccountRepository
+import dsm.service.announcement.core.domain.repository.AnnouncementRepository
+import dsm.service.announcement.core.usecase.UseCase
+import org.springframework.data.domain.PageRequest
+
+class SearchAnnouncementsUseCase(
+        private val announcementRepository: AnnouncementRepository,
+        private val accountRepository: AccountRepository
+) : UseCase<SearchAnnouncementsUseCase.InputValues, SearchAnnouncementsUseCase.OutputValues>() {
+    override fun execute(input: InputValues): OutputValues = OutputValues(getSearchAnnouncements(input))
+
+    private fun getSearchAnnouncements(input: InputValues): Pair<MutableIterable<Announcement>, Long>? {
+        return when (input.type) {
+            "club" -> {
+                Pair(announcementRepository
+                        .findByTitleContainsAndTypeOrderByDateDesc(
+                                title = input.query,
+                                type = input.type,
+                                pageable = PageRequest.of(input.start, input.count)),
+                        announcementRepository.countByTitleContainsAndType(
+                                title = input.query,
+                                type = input.type
+                        ))
+            }
+            "school" -> {
+                accountRepository.findByUuid(input.writerUuid, input.writerUuid)
+                        ?.let {
+                            Pair(announcementRepository
+                                    .findByTitleContainsAndTypeAndTargetGradeContainsAndTargetGroupContainsOrderByDateDesc(
+                                            title = input.query,
+                                            type = "school",
+                                            targetGrade = it.grade.toString(),
+                                            targetGroup = it.group.toString(),
+                                            pageable = PageRequest.of(input.start, input.count)),
+                                    announcementRepository.countByTitleContainsAndType(
+                                            title = input.query,
+                                            type = input.type))
+                        }
+            }
+            else -> throw BadRequestException()
+        }
+    }
+
+    class InputValues(
+            val writerUuid: String,
+            val query: String,
+            val start: Int,
+            val count: Int,
+            val type: String
+    ) : UseCase.InputValues
+
+    class OutputValues(
+            val pair: Pair<MutableIterable<Announcement>, Long>?
+    ) : UseCase.OutputValues
+}
