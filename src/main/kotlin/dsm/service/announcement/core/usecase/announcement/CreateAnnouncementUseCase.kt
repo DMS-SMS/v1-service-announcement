ef0a006be86802f50c4516c16ab01e13e03003a4
Maly <migsking@naver.com>
2021-01-05 11:48:30 +0900

    [MODIFY] CreateAnnouncement UseCase

--- src/main/kotlin/dsm/service/announcement/core/usecase/announcement/CreateAnnouncementUseCase.kt
+++ src/main/kotlin/dsm/service/announcement/core/usecase/announcement/CreateAnnouncementUseCase.kt
@@ -1,6 +1,7 @@
 package dsm.service.announcement.core.usecase.announcement
 
 import dsm.service.announcement.core.domain.entity.Announcement
+import dsm.service.announcement.core.domain.exception.ServerException
 import dsm.service.announcement.core.domain.repository.AnnouncementRepository
 import dsm.service.announcement.core.domain.repository.ClubRepository
 import dsm.service.announcement.core.usecase.UseCase
@@ -14,39 +15,37 @@ class CreateAnnouncementUseCase(
         private val announcementRepository: AnnouncementRepository,
         private val clubRepository: ClubRepository
 ): UseCase<CreateAnnouncementUseCase.InputValues, CreateAnnouncementUseCase.OutputValues>() {
-    override fun execute(input: InputValues): OutputValues =
-        OutputValues(announcementRepository.persist(createAnnouncement(input)).uuid)
+    override fun execute(input: InputValues): OutputValues {
+        val announcementUuid = announcementRepository.persist(createAnnouncement(input)).uuid?:
+                throw ServerException(message = "uuid isn't generated")
+        return OutputValues(announcementUuid)
+    }
 
     private fun createAnnouncement(input: InputValues): Announcement {
-        return Announcement(
-                uuid = createAnnouncementUuid(),
-                number = null,
-                writerUuid = input.writerUuid,
-                date = LocalDateTime.now(),
-                title = input.title,
-                targetGrade = input.targetGrade,
-                targetClass = input.targetGroup,
-                type = input.type,
-                club = getClubName(input),
-                content = input.content,
-                readAccounts = emptyList()
+        return generateAnnouncementUuid(
+                Announcement(
+                    uuid = null,
+                    number = null,
+                    writerUuid = input.writerUuid,
+                    date = LocalDateTime.now(),
+                    title = input.title,
+                    targetGrade = input.targetGrade,
+                    targetClass = input.targetGroup,
+                    type = input.type,
+                    club = getClubName(input),
+                    content = input.content,
+                    readAccounts = emptyList()
+            )
         )
     }
 
-    private fun createAnnouncementUuid(): String {
+    private fun generateAnnouncementUuid(announcement: Announcement): Announcement {
         while(true) {
-            val key = generateRandomKey()
-            val announcementUuid = "announcement-$key"
-            announcementRepository.findById(announcementUuid)?: return announcementUuid
+            val generatedAnnouncement = announcement.generateUuid()
+            val announcementUuid = generatedAnnouncement.uuid ?:
+            throw ServerException(message = "uuid isn't generated")
+            announcementRepository.findById(announcementUuid)?: return generatedAnnouncement
         }
-    }
-
-    private fun generateRandomKey(): String {
-        val source = "1234567890"
-        return Random().ints(12, 0, source.length)
-                .asSequence()
-                .map(source::get)
-                .joinToString("")
     }
 
     private fun getClubName(input: InputValues): String? {
