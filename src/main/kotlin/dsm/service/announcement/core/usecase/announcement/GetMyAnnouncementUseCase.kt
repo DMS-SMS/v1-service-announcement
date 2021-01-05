9789dec0eeab5c84917b98383de97b007dd9bba3
kangsinhee <rkdtlsgml40@naver.com>
2021-01-05 23:18:58 +0900

    [ADD] Get MyAnnouncement UseCase

---
+++ src/main/kotlin/dsm/service/announcement/core/usecase/announcement/GetMyAnnouncementUseCase.kt
@@ -0,0 +1,35 @@
+package dsm.service.announcement.core.usecase.announcement
+
+import dsm.service.announcement.core.domain.entity.Announcement
+import dsm.service.announcement.core.domain.repository.AnnouncementRepository
+import dsm.service.announcement.core.usecase.UseCase
+import org.springframework.data.domain.PageRequest
+import org.springframework.stereotype.Component
+
+@Component
+class GetMyAnnouncementUseCase(
+        private val announcementRepository: AnnouncementRepository
+): UseCase<GetMyAnnouncementUseCase.InputValues, GetMyAnnouncementUseCase.OutputValues>() {
+    override fun execute(input: InputValues): OutputValues = generateOutputValue(input)
+
+    private fun generateOutputValue(input: InputValues): OutputValues {
+        return OutputValues(
+                announcementRepository
+                        .findByWriterUuidAndTypeOrderByDateDesc(
+                                writerUuid = input.writerUuid,
+                                type = "school",
+                                pageable = PageRequest.of(input.start, input.count)),
+                announcementRepository.countByWriterUuidAndType(input.writerUuid, "school"))
+    }
+
+    class InputValues(
+            val writerUuid: String,
+            val start: Int,
+            val count: Int
+    ): UseCase.InputValues
+
+    class OutputValues(
+            val announcements: MutableIterable<Announcement>,
+            val count: Long
+    ): UseCase.OutputValues
+}
