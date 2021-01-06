c629659ab0f7341a71dd9b14a95d45a99640994b
Maly <migsking@naver.com>
2021-01-06 11:04:12 +0900

    [CREATE] Get AnnouncementDetail UseCase

--- src/main/kotlin/dsm/service/announcement/core/usecase/announcement/GetAnnouncementDetailUseCase.kt
+++ src/main/kotlin/dsm/service/announcement/core/usecase/announcement/GetAnnouncementDetailUseCase.kt
@@ -1,2 +1,30 @@
 package dsm.service.announcement.core.usecase.announcement
 
+import dsm.service.announcement.core.domain.entity.Account
+import dsm.service.announcement.core.domain.entity.Announcement
+import dsm.service.announcement.core.domain.exception.NotFoundException
+import dsm.service.announcement.core.domain.repository.AccountRepository
+import dsm.service.announcement.core.domain.repository.AnnouncementRepository
+import dsm.service.announcement.core.usecase.UseCase
+import org.springframework.stereotype.Component
+
+@Component
+class GetAnnouncementDetailUseCase(
+        private val announcementRepository: AnnouncementRepository
+): UseCase<GetAnnouncementDetailUseCase.InputValues, GetAnnouncementDetailUseCase.OutputValues>() {
+    override fun execute(input: InputValues): OutputValues =
+            OutputValues(getAnnouncement(input))
+
+    private fun getAnnouncement(input: InputValues): Announcement {
+        return announcementRepository.findById(input.announcementUuid)?:
+                throw NotFoundException(message = "Not found Announcement")
+    }
+
+    class InputValues(
+            val announcementUuid: String
+    ): UseCase.InputValues
+
+    class OutputValues(
+            val announcement: Announcement
+    ): UseCase.OutputValues
+}
