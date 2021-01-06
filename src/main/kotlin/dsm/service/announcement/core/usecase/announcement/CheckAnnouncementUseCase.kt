d6bb8a88bb43399d7f388444b26ca930bdd6eb87
Maly <migsking@naver.com>
2021-01-06 17:18:38 +0900

    [ADD] Check Announcement Logic

--- src/main/kotlin/dsm/service/announcement/core/usecase/announcement/CheckAnnouncementUseCase.kt
+++ src/main/kotlin/dsm/service/announcement/core/usecase/announcement/CheckAnnouncementUseCase.kt
@@ -1,32 +1,60 @@
 package dsm.service.announcement.core.usecase.announcement
 
+import dsm.service.announcement.core.domain.entity.Account
+import dsm.service.announcement.core.domain.entity.Announcement
+import dsm.service.announcement.core.domain.entity.enums.AccountType
+import dsm.service.announcement.core.domain.exception.BadRequestException
+import dsm.service.announcement.core.domain.exception.ServerException
+import dsm.service.announcement.core.domain.exception.UnAuthorizedException
 import dsm.service.announcement.core.domain.repository.AnnouncementRepository
 import dsm.service.announcement.core.usecase.UseCase
 import org.springframework.stereotype.Component
 
 @Component
 class CheckAnnouncementUseCase(
-        private val announcementRepository: AnnouncementRepository
+        private val announcementRepository: AnnouncementRepository,
+
+        private val getAccountUseCase: GetAccountUseCase
 ): UseCase<CheckAnnouncementUseCase.InputValues, CheckAnnouncementUseCase.OutputValues>() {
     override fun execute(input: InputValues): OutputValues {
-        return if (isAnnouncementCheck(input)) OutputValues(1)
-        else OutputValues(0)
+        return OutputValues(
+                isAnnouncementCheck(input, "club"),
+                isAnnouncementCheck(input, "school")
+        )
     }
 
-    private fun isAnnouncementCheck(input: InputValues): Boolean {
-        val announcements = announcementRepository.findByTypeOrderByDateDesc(input.type)
-        for (announcement in announcements) {
-            return announcement.isCheck.toInt() == 0
+    private fun isAnnouncementCheck(input: InputValues, type: String): Boolean {
+        val account = getStudent(input.uuid)
+
+        return when (type) {
+            "club" ->  checkReadAnnouncement(input.uuid, announcementRepository.findByTypeOrderByDateDesc(type))
+            "school" -> checkReadAnnouncement(
+                    input.uuid,
+                    announcementRepository.findByTypeAndTargetGradeContainsAndTargetGroupContainsOrderByDateDesc(
+                            type, account.grade.toString(), account.group.toString()))
+            else -> throw ServerException(message = "This type is not support.")
         }
-        return announcements.count() != 0
+    }
+
+    private fun getStudent(uuid: String): Account {
+        val account = getAccountUseCase.execute(GetAccountUseCase.InputValues(uuid)).account?:
+        throw UnAuthorizedException(message = "Your not student")
+        if (account.type != AccountType.STUDENT) throw UnAuthorizedException(message = "Your not student")
+        return account
+    }
+
+    private fun checkReadAnnouncement(studentUuid: String, announcements: MutableIterable<Announcement>): Boolean {
+        for (announcement: Announcement in announcements) {
+            if (!announcement.readAccounts.contains(studentUuid)) return true }
+        return false
     }
 
     class InputValues(
-            val writerUuid: String,
-            val type: String
+            val uuid: String
     ): UseCase.InputValues
 
     class OutputValues(
-            val isCheck: Long
+            val clubIsCheck: Boolean,
+            val schoolIsCheck: Boolean
     ): UseCase.OutputValues
 }
