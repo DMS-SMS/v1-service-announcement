9c1eaac2451bd2e40923649c26e2e0ede59f5430
kangsinhee <rkdtlsgml40@naver.com>
2021-01-06 15:34:40 +0900

    [ADD] Get Announcements I/O Mapper

---
+++ src/main/kotlin/dsm/service/announcement/controller/mapper/GetAnnouncementsOutputMapper.kt
@@ -0,0 +1,43 @@
+package dsm.service.announcement.controller.mapper
+
+import dsm.service.announcement.core.domain.entity.Announcement
+import dsm.service.announcement.core.usecase.announcement.GetAccountUseCase
+import dsm.service.announcement.core.usecase.announcement.GetAnnouncementsUseCase
+import dsm.service.announcement.proto.AnnouncementPreview
+import dsm.service.announcement.proto.GetAnnouncementsResponse
+import org.springframework.stereotype.Component
+import java.sql.Timestamp
+
+@Component
+class GetAnnouncementsOutputMapper(
+        private val getAccountUseCase: GetAccountUseCase
+): Mapper<GetAnnouncementsUseCase.OutputValues, GetAnnouncementsResponse>() {
+    override fun map(input: GetAnnouncementsUseCase.OutputValues): GetAnnouncementsResponse {
+        return GetAnnouncementsResponse
+                .newBuilder()
+                .setSize(input.count)
+                .addAnnouncement(mapItems(input.announcements))
+                .setStatus(200)
+                .build()
+    }
+
+    private fun mapItems(announcements: MutableIterable<Announcement>): AnnouncementPreview {
+        val previewBuilder = AnnouncementPreview.newBuilder()
+        for (announcement in announcements) {
+            previewBuilder
+                    .setAnnouncementId(announcement.uuid)
+                    .setTitle(announcement.title)
+                    .setDate(Timestamp.valueOf(announcement.date).time)
+                    .setIsChecked(announcement.isCheck)
+
+            getAccountUseCase.execute(GetAccountUseCase.InputValues(announcement.writerUuid)).account
+                    ?.let {
+                        announcement.readAccounts.count()
+                                .let { count -> previewBuilder.setViews(count.toLong()) }
+                        previewBuilder.setWriterName(it.name)
+                    }
+            announcement.number?.let { previewBuilder.setNumber(it) }
+        }
+        return previewBuilder.build()
+    }
+}
