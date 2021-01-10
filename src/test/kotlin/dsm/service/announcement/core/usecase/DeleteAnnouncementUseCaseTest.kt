1fa8200b409176d05478cd6b4881407ba3369d3a
Mallycrip <migsking@naver.com>
2021-01-10 20:33:24 +0900

    [ADD] 케이스 추가 (Notfound , UnAuthorized)

--- src/test/kotlin/dsm/service/announcement/core/usecase/DeleteAnnouncementUseCaseTest.kt
+++ src/test/kotlin/dsm/service/announcement/core/usecase/DeleteAnnouncementUseCaseTest.kt
@@ -1,9 +1,12 @@
 package dsm.service.announcement.core.usecase
 
 import dsm.service.announcement.core.domain.entity.Announcement
+import dsm.service.announcement.core.domain.exception.NotFoundException
+import dsm.service.announcement.core.domain.exception.UnAuthorizedException
 import dsm.service.announcement.core.domain.repository.AnnouncementRepository
 import dsm.service.announcement.core.usecase.announcement.DeleteAnnouncementUseCase
 import junit.framework.Assert.assertEquals
+import org.junit.Assert.assertThrows
 import org.junit.Test
 import org.mockito.ArgumentMatchers.any
 import org.mockito.ArgumentMatchers.anyString
@@ -17,7 +20,7 @@ class DeleteAnnouncementUseCaseTest: UseCaseTest() {
     @InjectMocks private lateinit var deleteAnnouncementUseCase: DeleteAnnouncementUseCase
     @Mock private lateinit var announcementRepository: AnnouncementRepository
 
-    @Test fun testMyAnnouncement() {
+    @Test fun testDeleteMyAnnouncement() {
         val input = DeleteAnnouncementUseCase.InputValues(
                 writerUuid = "teacher-111122223333",
                 announcementUuid = "announcement-111122223333"
@@ -42,6 +45,38 @@ class DeleteAnnouncementUseCaseTest: UseCaseTest() {
         val output = deleteAnnouncementUseCase.execute(input)
 
         assertEquals(output.announcement.uuid, "announcement-111122223333")
+    }
+
+    @Test fun testDeleteOtherAnnouncement() {
+        val input = DeleteAnnouncementUseCase.InputValues(
+                writerUuid = "teacher-111122223333",
+                announcementUuid = "announcement-111122223333"
+        )
+        given(announcementRepository.findById(anyString())).willReturn(Announcement(
+                uuid = "announcement-111122223333",
+                number = 1,
+                writerUuid = "teacher-111122223334",
+                writerName = "김선생",
+                date = LocalDateTime.now(),
+                title = "공지",
+                targetGrade = "1",
+                targetClass = "1",
+                type = "school",
+                club = null,
+                content = "{'content':'contents}",
+                readAccounts = arrayListOf("student-111122223334"),
+                isCheck = false
+        ))
+        assertThrows(UnAuthorizedException::class.java) { deleteAnnouncementUseCase.execute(input) }
+    }
+
+    @Test fun testDeleteNotExistAnnouncement() {
+        val input = DeleteAnnouncementUseCase.InputValues(
+                writerUuid = "teacher-111122223333",
+                announcementUuid = "announcement-111122223333"
+        )
+        given(announcementRepository.findById(anyString())).willReturn(null)
+        assertThrows(NotFoundException::class.java) { deleteAnnouncementUseCase.execute(input) }
     }
 
     private fun <T> any(): T {
