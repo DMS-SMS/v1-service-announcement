722f3d8c7767605740dd767f5789f19d79ae66cc
kangsinhee <rkdtlsgml40@naver.com>
2021-01-12 16:21:28 +0900

    [MODIFY] Get MyAnnouncement UseCase 권한 제한

--- src/main/kotlin/dsm/service/announcement/core/usecase/announcement/GetMyAnnouncementsUseCase.kt
+++ src/main/kotlin/dsm/service/announcement/core/usecase/announcement/GetMyAnnouncementsUseCase.kt
@@ -1,6 +1,9 @@
 package dsm.service.announcement.core.usecase.announcement
 
 import dsm.service.announcement.core.domain.entity.Announcement
+import dsm.service.announcement.core.domain.entity.enums.AccountType
+import dsm.service.announcement.core.domain.exception.NotFoundException
+import dsm.service.announcement.core.domain.exception.UnAuthorizedException
 import dsm.service.announcement.core.domain.repository.AnnouncementRepository
 import dsm.service.announcement.core.usecase.UseCase
 import org.springframework.data.domain.PageRequest
@@ -8,11 +11,13 @@ import org.springframework.stereotype.Component
 
 @Component
 class GetMyAnnouncementsUseCase(
-        private val announcementRepository: AnnouncementRepository
+        private val announcementRepository: AnnouncementRepository,
+        private val getAccountUseCase: GetAccountUseCase
 ): UseCase<GetMyAnnouncementsUseCase.InputValues, GetMyAnnouncementsUseCase.OutputValues>() {
     override fun execute(input: InputValues): OutputValues = generateOutputValue(input)
 
     private fun generateOutputValue(input: InputValues): OutputValues {
+        checkTeacher(input.writerUuid)
         return OutputValues(
                 announcementRepository
                         .findByWriterUuidAndTypeOrderByDateDesc(
@@ -20,6 +25,12 @@ class GetMyAnnouncementsUseCase(
                                 type = "school",
                                 pageable = PageRequest.of(input.start, input.count)),
                 announcementRepository.countByWriterUuidAndType(input.writerUuid, "school"))
+    }
+
+    private fun checkTeacher(writerUuid: String) {
+        getAccountUseCase.execute(GetAccountUseCase.InputValues(writerUuid)).account
+                ?.also { if (it.type != AccountType.TEACHER) throw UnAuthorizedException() }
+                ?: throw UnAuthorizedException()
     }
 
     class InputValues(
