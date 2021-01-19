efb3a73ccbbb6f4327e14e0e48e3c5a87295855b
Mallycrip <migsking@naver.com>
2021-01-20 02:59:01 +0900

    [FIX] MySQL Interceptor

--- src/main/kotlin/dsm/service/announcement/infra/config/db/mysql/MysqlInterceptor.kt
+++ src/main/kotlin/dsm/service/announcement/infra/config/db/mysql/MysqlInterceptor.kt
@@ -9,19 +9,19 @@ import java.io.Serializable
 
 class MysqlInterceptor: EmptyInterceptor() {
     override fun beforeTransactionCompletion(tx: Transaction?) {
-        val jaegerHandler = getBean("jaegerHandler") as JaegerService
+        val jaegerHandler = getBean("jaegerService") as JaegerService
         jaegerHandler.tracingStart("SQL (Transaction)")
         super.beforeTransactionCompletion(tx)
     }
 
     override fun afterTransactionCompletion(tx: Transaction?) {
-        val jaegerHandler = getBean("jaegerHandler") as JaegerService
+        val jaegerHandler = getBean("jaegerService") as JaegerService
         jaegerHandler.tracingEnd()
         super.afterTransactionCompletion(tx)
     }
 
     override fun onLoad(entity: Any?, id: Serializable?, state: Array<out Any>?, propertyNames: Array<out String>?, types: Array<out Type>?): Boolean {
-        val jaegerHandler = getBean("jaegerHandler") as JaegerService
+        val jaegerHandler = getBean("jaegerService") as JaegerService
         jaegerHandler.tracingStart("SQL (Select)")
         jaegerHandler.tracingEnd()
         return super.onLoad(entity, id, state, propertyNames, types)
