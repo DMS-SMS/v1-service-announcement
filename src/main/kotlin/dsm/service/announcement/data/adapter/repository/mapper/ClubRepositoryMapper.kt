dd947e49c59d3315f132652c65de7971e3600832
Mallycrip <migsking@naver.com>
2021-01-20 02:15:49 +0900

    [ADD] Annotation

--- src/main/kotlin/dsm/service/announcement/data/adapter/repository/mapper/ClubRepositoryMapper.kt
+++ src/main/kotlin/dsm/service/announcement/data/adapter/repository/mapper/ClubRepositoryMapper.kt
@@ -3,7 +3,9 @@ package dsm.service.announcement.data.adapter.repository.mapper
 import dsm.service.announcement.core.domain.entity.Club
 import dsm.service.announcement.proto.GetClubInformWithUUIDResponse
 import dsm.service.announcement.proto.GetClubUUIDWithLeaderUUIDResponse
+import org.springframework.stereotype.Component
 
+@Component
 class ClubRepositoryMapper {
     fun map(proto: GetClubUUIDWithLeaderUUIDResponse?): String? {
         if (proto == null) return null
