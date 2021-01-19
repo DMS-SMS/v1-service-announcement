e9913fa9f1d1f5364f3a093ca3a699edd94f009c
Mallycrip <migsking@naver.com>
2021-01-19 20:51:28 +0900

    [ADD] Club Repository Mapper

--- src/main/kotlin/dsm/service/announcement/data/adapter/repository/mapper/ClubRepositoryMapper.kt
+++ src/main/kotlin/dsm/service/announcement/data/adapter/repository/mapper/ClubRepositoryMapper.kt
@@ -1,2 +1,21 @@
 package dsm.service.announcement.data.adapter.repository.mapper
 
+import dsm.service.announcement.core.domain.entity.Club
+import dsm.service.announcement.proto.GetClubInformWithUUIDResponse
+import dsm.service.announcement.proto.GetClubUUIDWithLeaderUUIDResponse
+
+class ClubRepositoryMapper {
+    fun map(proto: GetClubUUIDWithLeaderUUIDResponse?): String? {
+        if (proto == null) return null
+        return proto.clubUUID
+    }
+
+    fun map(proto: GetClubInformWithUUIDResponse?): Club? {
+        if (proto == null) return null
+        return Club(
+            name = proto.name,
+            clubConcept = proto.clubConcept,
+            introduction = proto.introduction
+        )
+    }
+}
