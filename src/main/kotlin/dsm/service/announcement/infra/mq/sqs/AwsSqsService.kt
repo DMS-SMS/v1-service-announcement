5ea53864a0077a78ef6f4fcc4ee53a87e905c81a
Mallycrip <migsking@naver.com>
2021-01-26 21:39:08 +0900

    [DEBUG]

--- src/main/kotlin/dsm/service/announcement/infra/mq/sqs/AwsSqsService.kt
+++ src/main/kotlin/dsm/service/announcement/infra/mq/sqs/AwsSqsService.kt
@@ -1,11 +1,11 @@
 package dsm.service.announcement.infra.mq.sqs
 
-import org.springframework.stereotype.Component
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener
 import org.springframework.messaging.handler.annotation.Header
+import org.springframework.web.bind.annotation.RestController
 
 
-@Component
+@RestController
 class AwsSqsService {
     @SqsListener("\${cloud.aws.sqs.queue.url}")
     fun receive(message: String?, @Header("SenderId") senderId: String?) {
